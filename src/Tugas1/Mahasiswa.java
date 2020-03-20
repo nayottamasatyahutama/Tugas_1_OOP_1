c5f477c535c4ed7d806ad4ba21aa726133f8b2d5
Nayottama Satya Hutama <satyahutamanayottama@gmail.com>
2020-03-20 16:11:38 +0700

    Tugas_1_OOP_1 Nayottama Satya Hutama 18090004 4C

---
+++ src/Tugas1/Mahasiswa.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas1;
+
+/**
+ *
+ * @author Nayo
+ */
+public class Mahasiswa {
+    String nama, nim, kelas;
+   
+    public void print () {
+        System.err.println("Nama : "+nama+"\nNIM : "+nim+"\nkelas: "+kelas);
+    }
+}
---
+++ src/Tugas1/inputDataMahasiswa.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas1;
+
+/**
+ *
+ * @author Nayo
+ */
+public class inputDataMahasiswa {
+    public static void main(String args []) {
+        Mahasiswa mhs = new Mahasiswa();
+        
+        mhs.nama = "Nayottama Satya Hutama";
+        mhs.nim = "18090004";
+        mhs.kelas = "4C";
+        
+        mhs.print();
+        
+        
+    }
+}
